f50b0c46be95569e3e8e55213dbf9c330b4cd2fc
RenatoMAP77 <renatomapbusiness@gmail.com>
2024-04-25 10:32:33 -0300

    Construção SpringBoot do projeto #41

---
+++ src/back/wheelson/wheelson/src/main/java/com/renatomatos/wheelson/WheelsonApplication.java
@@ -0,0 +1,13 @@
+package com.renatomatos.wheelson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelsonApplication.class, args);
+	}
+
+}
---
+++ src/back/wheelson/wheelson/src/test/java/com/renatomatos/wheelson/WheelsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatomatos.wheelson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
