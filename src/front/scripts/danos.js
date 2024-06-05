a80f98dbe23065569061cef6fbb5861fd8d6d655
RenatoMAP77 <renatomapbusiness@gmail.com>
2024-06-05 09:45:39 -0300

    Formulário de danos funcionando

--- src/front/scripts/danos.js
+++ src/front/scripts/danos.js
@@ -2,9 +2,10 @@ const enviarbtn = document.getElementById('enviarbtn');
 
 
 const urlParams = new URLSearchParams(window.location.search);
-const id_aluguel = urlParams.get('id_aluguel');
-
-enviarbtn.addEventListener('click', async (event) => {
+const id_aluguel = urlParams.get('aluguel');
+console.log(id_aluguel);
+enviarbtn.addEventListener('click', function(event) {
+    event.preventDefault();
     // Fazer caso queira conferir se aluguel existe
     // fetch('http://localhost:8080/aluguel/${}', {
     //     method: 'get',
@@ -17,33 +18,33 @@ enviarbtn.addEventListener('click', async (event) => {
     // })
 
     var descricao = document.getElementById('descricao').value;
-            // Obter a data atual
+            
             const dataAtual = new Date();
-
-            // Extrair o ano, mês e dia
             const ano = dataAtual.getFullYear();
-            // O mês é retornado de 0 a 11, então precisamos adicionar 1 para obter o valor correto
-            const mes = (dataAtual.getMonth() + 1).toString().padStart(2, '0'); // Adiciona zero à esquerda se for menor que 10
-            const dia = dataAtual.getDate().toString().padStart(2, '0'); // Adiciona zero à esquerda se for menor que 10
+            const mes = (dataAtual.getMonth() + 1).toString().padStart(2, '0'); 
+            const dia = dataAtual.getDate().toString().padStart(2, '0'); 
     
-            // Formatar a data no formato desejado "xxxx-yy-zz"
+            
             const dataFormatada = `${ano}-${mes}-${dia}`;
     
-            console.log(dataFormatada); // Saída: "2024-05-13"
+            //console.log(dataFormatada); // Saída: "2024-05-13"
+
+            let corpo = {
+                descricao: descricao,
+                data: dataFormatada,
+                valorExtra: 0.0,
+                aluguel: { id_aluguel: id_aluguel }
+            }
+           // console.log(corpo);
+
     fetch(`http://localhost:8080/problema`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            descricao: descricao,
-            data: dataFormatada,
-            valorExtra: null,
-            aluguel: { id: id_aluguel }
-
-        }),
+        body: JSON.stringify(corpo),
     }).then(
-        //alert dizendo que foi enviado com sucesso
+        console.log(corpo),
         alert('Problema enviado com sucesso!'),
         
         window.location.href = "menuLocador.html"
