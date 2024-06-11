131c8575348037895cab4c6387f9d28f9c5490c6
Gabrielle Lira <79920434+GabiLira@users.noreply.github.com>
2024-06-11 09:20:07 -0300

    Update indicadores.js

--- src/front/scripts/indicadores.js
+++ src/front/scripts/indicadores.js
@@ -25,58 +25,59 @@ Dashboards.board('container', {
         connectors: [{
             id: 'taxaMensalAlugueis',
             type: 'JSON',
-            options: {
-                data: [
-                    ['Month', 'Taxa'],
-                    ['Jan', 20],
-                    ['Feb', 30],
-                    ['Mar', 40],
-                    ['Apr', 50],
-                    ['May', 60],
-                    ['Jun', 70],
-                    ['Jul', 80],
-                    ['Aug', 90],
-                    ['Sep', 100],
-                    ['Oct', 110],
-                    ['Nov', 120],
-                    ['Dec', 130]
-                ]
+            dataProcessor: function(connector) {
+                return new Promise((resolve, reject) => {
+                    fetch('/aluguel/taxaMensal')
+                        .then(response => response.json())
+                        .then(data => {
+                            resolve({
+                                data: data.map(item => [item.mes, item.taxa])
+                            });
+                        })
+                        .catch(error => reject(error));
+                });
             }
         }, {
             id: 'duracaoMediaReservas',
             type: 'JSON',
-            options: {
-                data: [
-                    ['Car', 'Average Duration'],
-                    ['Car 1', 10],
-                    ['Car 2', 20],
-                    ['Car 3', 30],
-                    ['Car 4', 40],
-                    ['Car 5', 50],
-                    ['Car 6', 60],
-                    ['Car 7', 70],
-                    ['Car 8', 80],
-                    ['Car 9', 90],
-                    ['Car 10', 100]
-                ]
+            dataProcessor: function(connector) {
+                return new Promise((resolve, reject) => {
+                    fetch('/aluguel')
+                        .then(response => response.json())
+                        .then(data => {
+                            const durations = data.map(aluguel => {
+                                const inicio = new Date(aluguel.inicio);
+                                const fim = new Date(aluguel.fim);
+                                return (fim - inicio) / (1000 * 60 * 60); // Duração em horas
+                            });
+                            const averageDuration = durations.reduce((acc, duration) => acc + duration, 0) / durations.length;
+                            resolve({
+                                data: [['Car', 'Average Duration'], ...durations.map((duration, index) => [`Car ${index + 1}`, duration])]
+                            });
+                        })
+                        .catch(error => reject(error));
+                });
             }
         }, {
             id: 'frequenciaAluguelPorVeiculo',
             type: 'JSON',
-            options: {
-                data: [
-                    ['Car', 'Frequency'],
-                    ['Car 1', 5],
-                    ['Car 2', 10],
-                    ['Car 3', 15],
-                    ['Car 4', 20],
-                    ['Car 5', 25],
-                    ['Car 6', 30],
-                    ['Car 7', 35],
-                    ['Car 8', 40],
-                    ['Car 9', 45],
-                    ['Car 10', 50]
-                ]
+            dataProcessor: function(connector) {
+                return new Promise((resolve, reject) => {
+                    fetch('/aluguel/alugueis')
+                        .then(response => response.json())
+                        .then(data => {
+                            const totalAlugueis = data.length;
+                            const alugueisPorCarro = data.reduce((acc, aluguel) => {
+                                acc[aluguel.carroId] = (acc[aluguel.carroId] || 0) + 1;
+                                return acc;
+                            }, {});
+                            const frequenciaAluguel = Object.keys(alugueisPorCarro).map(carroId => [`Car ${carroId}`, alugueisPorCarro[carroId] / totalAlugueis]);
+                            resolve({
+                                data: [['Car', 'Frequency'], ...frequenciaAluguel]
+                            });
+                        })
+                        .catch(error => reject(error));
+                });
             }
         }]
     },
@@ -131,24 +132,6 @@ Dashboards.board('container', {
             legend: {
                 enabled: false
             }
-        },
-        dataProcessor: function(connector) {
-            return new Promise((resolve, reject) => {
-                fetch('/aluguel')
-                    .then(response => response.json())
-                    .then(data => {
-                        const durations = data.map(aluguel => {
-                            const inicio = new Date(aluguel.inicio);
-                            const fim = new Date(aluguel.fim);
-                            return (fim - inicio) / (1000 * 60 * 60); // Duração em horas
-                        });
-                        const averageDuration = durations.reduce((acc, duration) => acc + duration, 0) / durations.length;
-                        resolve({
-                            data: [['Car', 'Average Duration'], ...durations.map((duration, index) => [`Car ${index + 1}`, duration])]
-                        });
-                    })
-                    .catch(error => reject(error));
-            });
         }
     }, {
         renderTo: 'dashboard-chart-3',
@@ -175,26 +158,8 @@ Dashboards.board('container', {
             legend: {
                 enabled: false
             }
-        },
-        dataProcessor: function(connector) {
-            return new Promise((resolve, reject) => {
-                fetch('/aluguel/alugueis')
-                    .then(response => response.json())
-                    .then(data => {
-                        const totalAlugueis = data.length;
-                        const alugueisPorCarro = data.reduce((acc, aluguel) => {
-                            acc[aluguel.carroId] = (acc[aluguel.carroId] || 0) + 1;
-                            return acc;
-                        }, {});
-                        const frequenciaAluguel = Object.keys(alugueisPorCarro).map(carroId => [`Car ${carroId}`, alugueisPorCarro[carroId] / totalAlugueis]);
-                        resolve({
-                            data: [['Car', 'Frequency'], ...frequenciaAluguel]
-                        });
-                    })
-                    .catch(error => reject(error));
-            });
         }
     }]
 }, true).then(dashboard => {
-    // Implementação final do dashboard
+    console.log('Dashboard initialized successfully');
 });
